cc523ffb2414924c2667a6e9dd30ef9c227c939a
sameer-shaikh <sameermubeen4@gmail.com>
2021-01-02 03:26:19 +0500

    Bug fixed in contact.js

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -113,7 +113,6 @@ const Contact = () => {
 										width: "100%",
 										height: "35px",
 										borderRadius: "5px",
-										border: "none",
 										border: "0.1px solid black",
 										outline: "0.5px",
 										fontSize: "20px",
